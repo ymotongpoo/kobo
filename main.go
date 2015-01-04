81f505af9f9baa73c5c97e11bbf0968f09484fdf
Yoshifumi YAMAGUCHI <ymotongpoo@gmail.com>
2015-01-04 20:48:59 +0900

    changed NewContentCrawler to return error via channel.

--- main.go
+++ main.go
@@ -20,6 +20,8 @@ const (
 	OldContentsURL     = `http://www.netriver.jp/rbs/usr/umoo/rivbb.cgi`
 	OldArchiveURL      = `http://baka.bakufu.org/kobokora/mee/index.html`
 	CustomUserAgent    = `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36`
+
+	QueueSize = 100
 )
 
 func main() {
@@ -36,20 +38,30 @@ func main() {
 func NewContentsCrawler(wg *sync.WaitGroup) {
 	dir := filepath.Join(os.TempDir(), "new")
 	_ = dir
-	queue := make(chan string, 100)
+	queue := make(chan string, QueueSize)
+	errCh := make(chan error, QueueSize)
 	go func() {
 		var wg sync.WaitGroup
 		for i := 0; i < 10; i++ {
 			wg.Add(1)
-			go NewContentsPageCrawler(&wg, i, queue)
+			go NewContentsPageCrawler(&wg, i, queue, errCh)
 		}
 		wg.Wait()
 		close(queue)
 	}()
 
-	for s := range queue {
-		p := path.Join(NewContentsBaseURL, s)
-		log.Println(p)
+LOOP:
+	for {
+		select {
+		case s, ok := <-queue:
+			if !ok {
+				break LOOP
+			}
+			p := path.Join(NewContentsBaseURL, s)
+			log.Println(p)
+		case err := <-errCh:
+			log.Println(err)
+		}
 	}
 	wg.Done()
 }
@@ -65,7 +77,7 @@ func OldArchiveCrawler(wg *sync.WaitGroup) {
 }
 
 // NewContentsPageCrawler extracts direct image file path in a page.
-func NewContentsPageCrawler(wg *sync.WaitGroup, page int, queue chan string) error {
+func NewContentsPageCrawler(wg *sync.WaitGroup, page int, queue chan string, errCh chan error) {
 	defer wg.Done()
 	value := url.Values{}
 	value.Add("page", strconv.Itoa(page))
@@ -73,14 +85,14 @@ func NewContentsPageCrawler(wg *sync.WaitGroup, page int, queue chan string) err
 
 	resp, err := CustomGet(urlStr)
 	if err != nil {
-		return err
+		errCh <- err
 	}
 	defer resp.Body.Close()
 
 	img := xmlpath.MustCompile(`//tbody//a/@href`)
 	root, err := xmlpath.ParseHTML(resp.Body)
 	if err != nil {
-		return err
+		errCh <- err
 	}
 	iter := img.Iter(root)
 	for iter.Next() {
@@ -90,7 +102,6 @@ func NewContentsPageCrawler(wg *sync.WaitGroup, page int, queue chan string) err
 			queue <- n.String()
 		}
 	}
-	return nil
 }
 
 // CustomGet replace default User-Agent header with custom one and call GET method.
